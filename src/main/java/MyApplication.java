ae77a799f3af47fae31eb7b898d0e5dd4bb826ad
Derek-huang <huangbc_6603@163.com>
2022-05-06 09:46:46 +0800

    submit springContextUtil

--- src/main/java/MyApplication.java
+++ src/main/java/MyApplication.java
@@ -1,5 +1,7 @@
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,11 @@ public class MyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
+    }
+
+    public void test(){
+        Assert.notNull("delivery", String.format("获取送货单异常，参数错误ID【%d】", "id"));
+        BeanUtils.copyProperties("delivery", "deliveryDTO");
     }
 
 }
