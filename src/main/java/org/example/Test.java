65b1bcd175944f28e218c61a1eba25cedf7d0d4d
Derek-huang <huangbc_6603@163.com>
2022-11-25 17:24:28 +0800

    update

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -15,17 +15,23 @@ public class Test {
     private static final Logger logger= LoggerFactory.getLogger(Test.class);
 
     public static void main(String[] args) {
-        System.out.println(RegExpUtil.isMobilePhoneNum("19865076603"));
-        System.out.println(RegExpUtil.isMobilePhoneNum("13684303359"));
+        System.out.println(RegExpUtil.isMobilePhoneNum("19865076633"));
+        System.out.println(RegExpUtil.isMobilePhoneNum("13684303366"));
         String jsonStr = JsonUtils.toJson(new User("Derek","18"));
         System.out.println(jsonStr);
         final Result<Void> test = test();
         System.out.println(test.getMsg());
         System.out.println(UuidUtil.getLongUUID());//598022427500945409
         System.out.println(UuidUtil.getStringUUID());//f3644ce2-a39e-4934-bb9c-a70b74e52843
+        logger.info(JsonUtils.toJson(test));
+        logger.info(JsonUtils.toJson(test1()));
     }
 
     public static Result<Void> test(){
         return Result.failureMsg("asd");
+    }
+    public static Result<User> test1(){
+        User user = new User("Derek", "18");
+        return Result.success(user);
     }
 }
