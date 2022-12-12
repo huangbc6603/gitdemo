44488ebe57da08d74b9b070d22dd6dc778a6edf7
Derek-huang <huangbc_6603@163.com>
2022-12-12 15:50:36 +0800

    SpringBootcontroller commit

---
+++ src/main/java/org/example/controller/controller.java
@@ -0,0 +1,34 @@
+package org.example.controller;
+
+import io.swagger.annotations.Api;
+import org.example.dto.Result;
+import org.example.entity.SysUser;
+import org.example.rest.SelectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Derek-huang
+ */
+@Api("SpringBootcontroller")
+@RestController
+@RequestMapping
+public class controller {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SelectService selectService;
+
+    @GetMapping("/select")
+    public Result<List<SysUser>> demo(){
+        List<SysUser> sysUsers = selectService.selectAllUser();
+        return Result.success(sysUsers);
+    }
+}
