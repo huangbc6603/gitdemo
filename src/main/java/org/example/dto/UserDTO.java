9d434064de073d665f4bc53bfe8e4f10218a7238
huangbc <huangbc_6603@163.com>
2023-05-26 08:43:23 +0800

    ApiModel使用,实体类注释

--- src/main/java/org/example/dto/UserDTO.java
+++ src/main/java/org/example/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package org.example.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
@@ -9,11 +11,14 @@ import javax.validation.constraints.NotBlank;
  * @author Derek-huang
  */
 @Data
+@ApiModel(value = "userDTO实体类",description = "接收前端传入的参数")
 public class UserDTO extends PageReq{
 
+    @ApiModelProperty(value = "用户名")
     @NotBlank(message = "昵称不能为空")
     private String name;
 
+    @ApiModelProperty(value = "用户密码")
     @Length(min = 6,max = 12,message = "密码最小6位,最大12位")
     private String password;
 
